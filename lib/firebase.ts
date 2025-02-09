7b74d0c17a147d0a8541e80bf5134d7bf44d9999
eengsf <shofiyulfuad797@gmail.com>
2025-02-10 06:58:56 +0700

    next commit

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,12 +8,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.PUBLIC_API_FIREBASE_API_KEY,
-  authDomain: process.env.PUBLIC_API_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.PUBLIC_API_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.PUBLIC_API_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.PUBLIC_API_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.PUBLIC_API_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_API_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_API_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_API_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_API_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_API_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
